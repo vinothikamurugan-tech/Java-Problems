fe6d93396c3f76b30948ee35d136529c521eabe1
Vinothika Murugan <vinothikamurugan@gmail.com>
2026-03-14 22:04:03 +0530

    Add files via upload

---
+++ Nested_forloops/pro7.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class pro7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i<=(n/2)+1 ; i++){
+            for(int j = 1 ; j<=i*2-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i = n/2 ; i>=1 ; i--){
+            for(int j = i*2-1 ; j>=1 ;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
