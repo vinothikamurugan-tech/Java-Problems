a75a871210d3dca55eb219af5461a28dc7033649
Vinothika Murugan <vinothikamurugan@gmail.com>
2026-03-15 22:39:36 +0530

    Add files via upload

---
+++ Nested_forloops/pro11.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class pro11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int c = 0;
+        for(int i = 0; i<n ; i++){
+            for(int j = 0; j<=i ; j++){
+                if(j==0 || j==i){
+                    c = 1;
+                    System.out.print(c+" ");
+                }else{
+                    c = c*(i-j+1)/j;
+                    System.out.print(c+" ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
