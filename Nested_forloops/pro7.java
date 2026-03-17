fbd38cbc5feefa081e760d5e777e05a2d4ee2e43
Vinothika Murugan <vinothikamurugan@gmail.com>
2026-03-17 22:18:50 +0530

    Update pro7.java

--- Nested_forloops/pro7.java
+++ Nested_forloops/pro7.java
@@ -7,7 +7,7 @@ public class pro7 {
             for(int j = 1 ; j<=i*2-1; j++){
                 System.out.print("*");
             }
-            System.out.println();
+            System.out.println(); 
         }
         for(int i = n/2 ; i>=1 ; i--){
             for(int j = i*2-1 ; j>=1 ;j--){
