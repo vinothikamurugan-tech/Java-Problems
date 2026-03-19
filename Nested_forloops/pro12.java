916ed0861ee66b594852bdd069b75b22d509b7b1
Vinothika Murugan <vinothikamurugan@gmail.com>
2026-03-19 20:22:59 +0530

    Update pro12.java

--- Nested_forloops/pro12.java
+++ Nested_forloops/pro12.java
@@ -6,7 +6,7 @@ public class pro12 {
         for(int i = 1; i<=n ; i++){
             if(i%2==1){
                 for(int j = 1; j<=n ; j++){
-                  if(j%2==1){
+                  if(j%2==1){ 
                       System.out.print("#"+" ");
                   }else{
                       System.out.print("@"+" ");
