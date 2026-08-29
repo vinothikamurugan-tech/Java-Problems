b9446a33154e91b9cf0abae0ab90f6d014fce733
Vinothika Murugan <vinothikamurugan@gmail.com>
2026-08-29 22:41:57 +0530

    Fix formatting issues in prog6.java

--- Java-Basics/prog6.java
+++ Java-Basics/prog6.java
@@ -6,7 +6,7 @@ public class prog6 {
         int mil = sc.nextInt();
         int fuel = sc.nextInt();
         int tool = sc.nextInt();
-        int km = dis/mil;
+        int km = dis/mil; 
         int a = km*fuel;
         int amount = a+tool;
         System.out.println("Total Trip Cost : "+amount);
